c3cfdf6c8c876a3f7c9e70424189006e3faeadc8
IDMOP0 <marcos.oriol@idneo.com>
2025-04-30 20:05:36 +0200

    feat: project start | read xlsx input files

---
+++ src/utils/load.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+
+def load_to_df(file_path: str) -> pd.DataFrame:
+    data = pd.read_excel(file_path, header=None, usecols=[0, 7, 11, 12, 13], names=["player", "result", "planets_combo", "intensity_degree", "planets_intensity"])
+
+    return data
